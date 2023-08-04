c5c70cf4d930c9dd62c983198d60dd813ee4ad68
spaceC00kie <kirstie317@gmail.com>
2023-08-04 17:10:06 -0400

    refined About Me paragraph in Intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,12 +5,12 @@ export const Intro: React.FC<Props> = () => {
     <div className="mx-2 flex flex-col gap-4 rounded-b-lg border-b-2 border-l-2 border-r-2 border-gray-500 bg-blue-950 p-8 text-gray-200">
       <p className="my-2 flex justify-center text-2xl font-bold">About Me</p>
       <p className="my-2 flex justify-center text-xl font-medium">
-        I am a motivated software developer with a strong foundation in
-        teamwork, leadership, and technology. I have excellent communication
-        skills and a passion for efficiency, and I am constantly seeking to
-        learn and grow in my field. With a background in customer service and
-        workflow principles, I have the skills and experience to excel in
-        software development roles.
+        I'd describe myself as an enthusiastic software developer with excellent
+        communication skills. Having created a strong foundation in teamwork,
+        leadership, and technology, I now seek to enter the field
+        professionally. My passion for efficiency and background in customer
+        service and workflow principles makes me uniquely prepared to contribute
+        to a diverse development team.
       </p>
     </div>
   )
